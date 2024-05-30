922d23c7dfd58523b970c90810f1276086071893
thainguyencoffee <nguyennt11032004@gmail.com>
2024-05-30 11:09:30 +0700

    up

--- src/main/java/com/bookshop/edgeservice/config/SecurityConfig.java
+++ src/main/java/com/bookshop/edgeservice/config/SecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.oauth2.client.web.server.ServerOAuth2Authori
 import org.springframework.security.oauth2.client.web.server.WebSessionServerOAuth2AuthorizedClientRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
-import org.springframework.security.web.server.authentication.RedirectServerAuthenticationEntryPoint;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
 import org.springframework.security.web.server.csrf.CsrfToken;
@@ -22,49 +21,49 @@ import reactor.core.publisher.Mono;
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-        @Bean
-        SecurityWebFilterChain securityWebFilterChain(
-                        ServerHttpSecurity http,
-                        ReactiveClientRegistrationRepository clientRegistrationRepository) {
-                return http
-                                .authorizeExchange(exchange -> exchange
-                                                .pathMatchers("/actuator/**").permitAll()
-                                                .pathMatchers("/", "/*.css", "/*.js", "/favicon.ico").permitAll()
-                                                .pathMatchers(HttpMethod.GET, "/books/**").permitAll()
-                                                .anyExchange().authenticated())
-                                .exceptionHandling(exceptionHandling -> exceptionHandling
-                                                .authenticationEntryPoint(new HttpStatusServerEntryPoint(
-                                                                HttpStatus.UNAUTHORIZED)))
-                                .oauth2Login(Customizer.withDefaults())
-                                .logout(logout -> logout.logoutSuccessHandler(
-                                                oidcLogoutSuccessHandler(clientRegistrationRepository)))
-                                .csrf(csrf -> csrf.csrfTokenRepository(
-                                                CookieServerCsrfTokenRepository.withHttpOnlyFalse()))
-                                .build();
-        }
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(
+            ServerHttpSecurity http,
+            ReactiveClientRegistrationRepository clientRegistrationRepository) {
+        return http
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/actuator/**").permitAll()
+                        .pathMatchers("/", "/*.css", "/*.js", "/favicon.ico").permitAll()
+                        .pathMatchers(HttpMethod.GET, "/books/**").permitAll()
+                        .anyExchange().authenticated())
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(new HttpStatusServerEntryPoint(
+                                HttpStatus.UNAUTHORIZED)))
+                .oauth2Login(Customizer.withDefaults())
+                .logout(logout -> logout.logoutSuccessHandler(
+                        oidcLogoutSuccessHandler(clientRegistrationRepository)))
+                .csrf(csrf -> csrf.csrfTokenRepository(
+                        CookieServerCsrfTokenRepository.withHttpOnlyFalse()))
+                .build();
+    }
 
-        @Bean
-        ServerOAuth2AuthorizedClientRepository authorizedClientRepository() {
-                return new WebSessionServerOAuth2AuthorizedClientRepository();
-        }
+    @Bean
+    ServerOAuth2AuthorizedClientRepository authorizedClientRepository() {
+        return new WebSessionServerOAuth2AuthorizedClientRepository();
+    }
 
-        private ServerLogoutSuccessHandler oidcLogoutSuccessHandler(
-                        ReactiveClientRegistrationRepository clientRegistrationRepository) {
-                var oidcLogoutSuccessHandler = new OidcClientInitiatedServerLogoutSuccessHandler(
-                                clientRegistrationRepository);
-                oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");
-                return oidcLogoutSuccessHandler;
-        }
+    private ServerLogoutSuccessHandler oidcLogoutSuccessHandler(
+            ReactiveClientRegistrationRepository clientRegistrationRepository) {
+        var oidcLogoutSuccessHandler = new OidcClientInitiatedServerLogoutSuccessHandler(
+                clientRegistrationRepository);
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");
+        return oidcLogoutSuccessHandler;
+    }
 
-        @Bean
-        WebFilter csrfWebFilter() {
-                return (exchange, chain) -> {
-                        exchange.getResponse().beforeCommit(() -> Mono.defer(() -> {
-                                Mono<CsrfToken> csrfToken = exchange.getAttribute(CsrfToken.class.getName());
-                                return csrfToken != null ? csrfToken.then() : Mono.empty();
-                        }));
-                        return chain.filter(exchange);
-                };
-        }
+    @Bean
+    WebFilter csrfWebFilter() {
+        return (exchange, chain) -> {
+            exchange.getResponse().beforeCommit(() -> Mono.defer(() -> {
+                Mono<CsrfToken> csrfToken = exchange.getAttribute(CsrfToken.class.getName());
+                return csrfToken != null ? csrfToken.then() : Mono.empty();
+            }));
+            return chain.filter(exchange);
+        };
+    }
 
 }
